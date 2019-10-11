5ce35611b71f1b575d3224c5ca7899a92a35dc0d
Lily <yyn605@outlook.com>
2019-10-11 09:27:35 -0700

    implemented functions for KDT: height, size, build, nearestNeighbor, and destructor. range_search is yet to be implemented

--- src/kdt/KDT.hpp
+++ src/kdt/KDT.hpp
@@ -56,10 +56,48 @@ class KDT {
     virtual ~KDT() { deleteAll(root); }
 
     /** TODO */
-    void build(vector<Point>& points) {}
+    void build(vector<Point>& points) {
+        if (points.empty()) {
+            root = 0;
+            isize = 0;
+            iheight = 0;
+            return;
+        }
+
+        numDim = points[0].numDim;
+        iheight = -1;
+        root = buildSubtree(points, 0, points.size(), 0, -1);
+        isize = points.size();
+    }
 
     /** TODO */
-    Point* findNearestNeighbor(Point& queryPoint) { return nullptr; }
+    Point* findNearestNeighbor(Point& queryPoint) {
+        if (isize == 0) {
+            return nullptr;
+        }
+
+        KDNode* curr = root;
+        KDNode* prev;
+        unsigned dim = 0;
+
+        while (curr != nullptr) {
+            prev = curr;
+            if (queryPoint.valueAt(dim) < curr->point.valueAt(dim)) {
+                curr = curr->left;
+            } else {
+                curr = curr->right;
+            }
+            dim = (dim + 1) % numDim;
+        }
+
+        prev->point.setDistToQuery(queryPoint);
+        nearestNeighbor = prev->point;
+        threshold = prev->point.distToQuery;
+
+        findNNHelper(root, queryPoint, 0);
+
+        return &nearestNeighbor;
+    }
 
     /** Extra credit */
     vector<Point> rangeSearch(vector<pair<double, double>>& queryRegion) {
@@ -67,20 +105,60 @@ class KDT {
     }
 
     /** TODO */
-    unsigned int size() const { return 0; }
+    unsigned int size() const { return isize; }
 
     /** TODO */
-    int height() const { return 0; }
+    int height() const { return iheight; }
 
   private:
     /** TODO */
     KDNode* buildSubtree(vector<Point>& points, unsigned int start,
                          unsigned int end, unsigned int curDim, int height) {
-        return nullptr;
+        if (start >= end) {
+            return nullptr;
+        }
+
+        iheight = (iheight < height + 1) ? height + 1 : iheight;
+
+        sort(points.begin(), points.end(), CompareValueAt(curDim));
+        unsigned int median = floor((start + end) / 2);
+        unsigned int nextDim = (curDim + 1) % numDim;
+
+        KDNode* curNode = new KDNode(points[median]);
+        curNode->left =
+            buildSubtree(points, start, median, nextDim, height + 1);
+        curNode->right =
+            buildSubtree(points, median + 1, end, nextDim, height + 1);
+        return curNode;
     }
 
     /** TODO */
-    void findNNHelper(KDNode* node, Point& queryPoint, unsigned int curDim) {}
+    void findNNHelper(KDNode* node, Point& queryPoint, unsigned int curDim) {
+        if (node == nullptr) {
+            return;
+        }
+
+        unsigned int nextDim = (curDim + 1) % numDim;
+        if (queryPoint.valueAt(curDim) < node->point.valueAt(curDim)) {
+            findNNHelper(node->left, queryPoint, nextDim);
+            if (pow(node->point.valueAt(curDim) - queryPoint.valueAt(curDim),
+                    2.0) <= threshold) {
+                findNNHelper(node->right, queryPoint, nextDim);
+            }
+        } else {
+            findNNHelper(node->right, queryPoint, nextDim);
+            if (pow(queryPoint.valueAt(curDim) - node->point.valueAt(curDim),
+                    2.0) <= threshold) {
+                findNNHelper(node->left, queryPoint, nextDim);
+            }
+        }
+
+        node->point.setDistToQuery(queryPoint);
+        if (node->point.distToQuery < threshold) {
+            nearestNeighbor = node->point;
+            threshold = node->point.distToQuery;
+        }
+    }
 
     /** Extra credit */
     void rangeSearchHelper(KDNode* node, vector<pair<double, double>>& curBB,
@@ -88,7 +166,14 @@ class KDT {
                            unsigned int curDim) {}
 
     /** TODO */
-    static void deleteAll(KDNode* n) {}
+    static void deleteAll(KDNode* n) {
+        if (n == nullptr) {
+            return;
+        }
+        deleteAll(n->left);
+        deleteAll(n->right);
+        delete n;
+    }
 
     // Add your own helper methods here
 };
